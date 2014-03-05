c755495b2b9e4be28950289bb3ef2a8f10b9fdba
TheBastedo <justin.bastedo@gmail.com>
2014-03-05 14:27:20 -0800

    Minor update, starting to add in some security, basic update functionality stubbed out

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(3000, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:3000/');
+/*
+ * Handle our requires
+ */
+var express = require('express');
+		updates = require('./routes/updates');
+
+/*
+ * Creating our main app
+ */
+var app = express();
+
+// Configure it
+app.configure(function() {
+	app.use(express.logger('dev'));
+	//app.use(express.logger('tiny'));
+	app.use(express.bodyParser());
+});
+
+/*
+ * Our Update Routes
+ */
+app.get('/updates', updates.getAll);
+app.get('/updates/:id', updates.findById);
+app.post('/updates', updates.addUpdate);
+app.put('/updates/:id', updates.updateUpdate);
+app.delete('/updates/:id', updates.deleteUpdate);
+
+app.listen(3000);
+console.log('listening on port 3000...');
