aa45f11948a6c3a3577ab491a77d1f2fefb50432
TheBastedo <justin.bastedo@gmail.com>
2014-03-08 16:25:33 -0800

    trying to get CORS headers sending out from the app

--- server.js
+++ server.js
@@ -24,7 +24,33 @@ app.configure(function() {
 	app.use(express.logger('dev'));
 	//app.use(express.logger('tiny'));
 	app.use(express.bodyParser());
-	app.use(allowCrossDomain);
+	//app.use(allowCrossDomain);
+	app.use(function(req, res, next) {
+    var oneof = false;
+    if(req.headers.origin) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-method']) {
+        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-headers']) {
+        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+        oneof = true;
+    }
+    if(oneof) {
+        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+    }
+
+    // intercept OPTIONS method
+    if (oneof && req.method == 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+	});
 });
 
 /*
